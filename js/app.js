2ab962f64735ef0531ef9ffbc6769ae91de78960
LeeJaein <devxoox1106@gmail.com>
2025-04-11 12:18:57 +0900

    feat : 삭제기능구현

--- js/app.js
+++ js/app.js
@@ -8,14 +8,22 @@ const ul = document.querySelector('ul');
  * 3. 삭제버튼 기능구현
  */
 
+const delItem = (event) => {
+  const target = event.target.parentElement;
+  target.remove();
+}
+
 const addItem = (text) => {
   const li = document.createElement('li');
   const button = document.createElement('button');
   const span = document.createElement('span');
   
   span.innerText = text;
+  button.innerText = '삭제';
+  button.addEventListener('click', delItem);
   
   li.appendChild(span);
+  li.appendChild(button);
   ul.appendChild(li);
 }
 
