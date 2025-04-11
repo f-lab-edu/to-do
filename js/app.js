e109ab1476569662bd8a58324183c1babd8d770e
LeeJaein <devxoox1106@gmail.com>
2025-04-11 22:09:19 +0900

    feat : 로컬스토리지기능구현

--- js/app.js
+++ js/app.js
@@ -8,33 +8,64 @@ const ul = document.querySelector('ul');
  * 3. 삭제버튼 기능구현
  */
 
+let todos = [];
+
+const save = () => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
 const delItem = (event) => {
   const target = event.target.parentElement;
+  
+  todos = todos.filter((todo) => todo.id !== parseInt(target.id));
+  save();
+
   target.remove();
 }
 
-const addItem = (text) => {
+const addItem = (todo) => {
   const li = document.createElement('li');
   const button = document.createElement('button');
   const span = document.createElement('span');
   
-  span.innerText = text;
+  span.innerText = todo.text;
   button.innerText = '삭제';
   button.addEventListener('click', delItem);
   
   li.appendChild(span);
   li.appendChild(button);
   ul.appendChild(li);
+  li.id = todo.id;
 }
 
 const handler = (event) => {
   event.preventDefault();
   
-  const toDo = input.value;
-  if(toDo !== ''){
-    addItem(toDo);
+  const todo = {
+    id : Date.now(),
+    text : input.value
+  }
+
+  todos.push(todo)
+  
+  if(todo.text !== ''){
+    addItem(todo);
+    save();
   }
   input.value = '';
 }
 
+const init = () => {
+  const userTodos = JSON.parse(localStorage.getItem('todos'));
+  
+  if(userTodos){
+    userTodos.array.forEach((todo) => {
+      addItem(todo);
+    });  
+  }
+  
+  todos = userTodos;
+}
+
+init();
 form.addEventListener("submit", handler);
