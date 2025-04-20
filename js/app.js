8ec93d2d95a2f0bfc143f8c02f083d3a4a6d3c3e
LeeJaein <devxoox1106@gmail.com>
2025-04-20 16:35:23 +0900

    fix: html 변경

--- js/app.js
+++ js/app.js
@@ -27,14 +27,29 @@ const addItem = (todo) => {
   const li = document.createElement('li');
   const button = document.createElement('button');
   const span = document.createElement('span');
+
+  li.classList.add('py-1');
+  li.classList.add('flex');
   
   span.innerText = todo.text;
+  span.classList.add('w-5/6');
+  span.classList.add('mr-1');
+  span.classList.add('border-1');
+  span.classList.add('border-gray-300');
+  span.classList.add('py-2');
+  span.classList.add('px-4');
+  span.classList.add('rounded');
+
   button.innerText = '삭제';
   button.addEventListener('click', delteItem);
-  button.classList.add('bg-violet-500');
-  button.classList.add('border-2');
+  
+  button.classList.add('bg-purple-300');
+  button.classList.add('hover:bg-purple-100');
   button.classList.add('border-solid');
-  button.classList.add('rounded-xl');
+  button.classList.add('text-white');
+  button.classList.add('py-2');
+  button.classList.add('px-4');
+  button.classList.add('rounded');
   
   li.appendChild(span);
   li.appendChild(button);
@@ -60,15 +75,18 @@ const saveTodo = (event) => {
 }
 
 const init = () => {
-  const originTodos = JSON.parse(localStorage.getItem('todos'));
   
-  if(originTodos){
-    originTodos.forEach((todo) => {
-      addItem(todo);
-    });  
-  }
-
-  todos = originTodos;
+  try {  
+    const originTodos = JSON.parse(localStorage.getItem('todos'));  
+     originTodos.forEach((todo) => {  
+        addItem(todo);  
+      });  
+      todos = originTodos;  
+  } catch {  
+      todos = [];
+      alert('오류가 발생했습니다');  
+  }  
+ 
 }
 
 init();
