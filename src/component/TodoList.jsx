bbe5a6bbda41b4a5bfced04a05975872d6cca968
LeeJaein <devxoox1106@gmail.com>
2025-05-10 09:09:37 +0900

    fix: 콘솔로그 삭제

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import TodoItem from './TodoItem'
 
 function TodoList({todos, deleteTodo}) {
-  console.log(' todos : ' , todos );
   return (
     <ul>
         {todos.map(todo => {
-            console.log(' todo check : ' , todo );
             return <TodoItem 
               key={todo.id}
               todo={todo}
