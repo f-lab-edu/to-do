30cf03635262954181f9ed47f8e396ddf7369cb5
LeeJaein <devxoox1106@gmail.com>
2025-09-15 16:24:49 +0900

    feat: 메인화면 페이징처리 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,6 +6,8 @@ import type { Todo } from '../types';
 
 function MainPage() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemPerPage = 10;
 
   useEffect(() => {
     const originTodos = localStorage.getItem('todos');
@@ -25,6 +27,13 @@ function MainPage() {
     }
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
+
+  const getCurrentTodos = () => {
+    const startIndex = (currentPage - 1) * itemPerPage;
+    return todos.slice(startIndex, startIndex + itemPerPage);
+  }
+
+  const totalPages = Math.ceil(todos.length / itemPerPage);
 
   const addTodo = (text: string): void => {
     if (text.trim() === '') {
@@ -48,14 +57,51 @@ function MainPage() {
     <div className='flex flex-col justify-center items-center'>
       <h1 className='m-8 text-2xl'>To Do List</h1>
       <TodoForm onSubmit={addTodo} />
-      <TodoList todos={todos} deleteTodo={deleteTodo} />
+      {/* <TodoList todos={todos} deleteTodo={deleteTodo} /> */}
+      <TodoList todos={getCurrentTodos()} deleteTodo={deleteTodo} />
 
-      <div>
-        <button className='mr-8 text-gray-400 '>이전</button>
-        <button className='text-gray-400 '>다음</button>
-      </div>
+      {totalPages > 1 && (
+        <div className='mt-4 flex items-center gap-2'>
+          <button 
+            className='mr-3 text-gray-400'
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            이전
+          </button>
+          
+          {/* 페이지 번호들 */}
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
+            <button
+              key={page}
+              className={`mr-3 p-8 rounded ${
+                currentPage === page 
+                  ? 'bg-orange-300 text-black' 
+                  : 'text-gray-600 hover:bg-gray-100'
+              }`}
+              onClick={() => setCurrentPage(page)}
+            >
+              {page}
+            </button>
+          ))}
+          
+          <button 
+            className='bg-pink-600'
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            다음
+          </button>
+        </div>
+      )}
     </div>
   );
+      
+      // <div>
+      //   <button className='mr-8 text-gray-400 '>이전</button>
+      //   <button className='text-gray-400 '>다음</button>
+      // </div>
+  
 }
 
 export default MainPage;
